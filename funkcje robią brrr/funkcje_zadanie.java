9f3a10d2353d4d463dfb9bf525e21ea59944514c
___ <89976459+fryyta@users.noreply.github.com>
2021-12-01 09:11:03 +0100

    Add files via upload

---
+++ funkcje robią brrr/funkcje_zadanie.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+
+class GenerateRandom {
+    public static void main( String args[] ) {
+      Random rand = new Random(); 
+      
+        
+      int a = rand.nextInt(1) ; 
+      int b = rand.nextInt(1) ; 
+      int c = rand.nextInt(1) ; 
+
+      int delta = 1 ;
+      int p = 1;
+      int q =1 ;
+      int x_0 =1 ;
+      int x_1 = 1;
+      int x_2 = 1;
+      
+      
+      
+      System.out.println("Random integer value from 0 to: "+ int_random);
+      
+    }
+}
